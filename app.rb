be85c9b0c2cd545da5f385763c3d5a9d0d404704
Araik7979 <aro797919@gamil.com>
2017-05-08 23:47:07 +0400

    Add  post and comment migration

--- app.rb
+++ app.rb
@@ -2,7 +2,18 @@ require'rubygems'
 require'sinatra'
 require'sinatra/reloader'
 require'sqlite3'
+require'sinatra/activerecord'
 
+set :database, "sqlite3:leprosorium.db"
+
+class Post < ActiveRecord::Base
+end
+
+class Comment < ActiveRecord::Base 
+end
+
+
+	
 get'/' do
 erb"HELLO"
 end
