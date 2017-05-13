0d963656e49feefca57974fa3d17a3b504d2c3dc
Araik7979 <aro797919@gamil.com>
2017-05-13 15:10:50 +0400

    Save post table and view posts

--- app.rb
+++ app.rb
@@ -6,18 +6,38 @@ require'sinatra/activerecord'
 
 set :database, "sqlite3:leprosorium.db"
 
-class Post < ActiveRecord::Base
+before do 
+	@posts = Post.all 
 end
 
-class Comment < ActiveRecord::Base 
+class Post < ActiveRecord::Base
+	
+end
+
+class Comment < ActiveRecord::Base
+ 
 end
 
 
 	
 get'/' do
-erb"HELLO"
+@posts = Post.all
+  erb :posts
 end
 
  get'/new' do
+ 	@c = Post.new
  	erb :new
  end
+
+ post'/new' do 
+ 	@c = Post.new params[:main]
+ 	if @c.save
+ 	erb"Спасибо"
+ 	else
+ 	@error = @c.errors.full_messages.first
+ 	erb :new
+ end
+
+ end
+
